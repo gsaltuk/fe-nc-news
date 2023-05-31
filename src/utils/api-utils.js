1b7f4a85d313a236a2e4150da171e0328f0826fa
gsaltuk <127306125+gsaltuk@users.noreply.github.com>
2023-05-31 09:59:30 +0100

    ticket 7 add voting functionality complete

--- src/utils/api-utils.js
+++ src/utils/api-utils.js
@@ -11,16 +11,45 @@ function fetchArticles() {
 }
 
 function fetchArticle(id) {
-    return articlesApi.get(`/articles/${id}`).then((res) => {
-        return res.data
-    })
-};
-
-
+  return articlesApi.get(`/articles/${id}`).then((res) => {
+    return res.data;
+  });
+}
 
 function fetchComments(id) {
-    return articlesApi.get(`/articles/${id}/comments`).then((res) => {
-        return res.data
-    })
+  return articlesApi.get(`/articles/${id}/comments`).then((res) => {
+    return res.data;
+  });
 }
-export { fetchArticles, fetchArticle, fetchComments };
+
+function increaseVote(id) {
+  const patchBody = { inc_votes: 1 };
+  return articlesApi
+    .patch(`/articles/${id}`, patchBody)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      return err;
+    });
+}
+
+function decreaseVote(id) {
+  const patchBody = { inc_votes: -1 };
+  return articlesApi
+    .patch(`/articles/${id}`, patchBody)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      return err;
+    });
+}
+
+export {
+  fetchArticles,
+  fetchArticle,
+  fetchComments,
+  increaseVote,
+  decreaseVote,
+};
