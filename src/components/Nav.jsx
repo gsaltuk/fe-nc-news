95cd5107372c178a7c32d94608ae3f3aa9a42378
gsaltuk <127306125+gsaltuk@users.noreply.github.com>
2023-06-02 15:40:03 +0100

    update to nav logo size

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,6 @@ function Nav({username, name, avatar_url}) {
                     <li className="nav-list-logo"><Link to="" ><img src='/chit-chat-logo-white.svg'></img></Link></li>
                     <li className="nav-list-item"><Link to="/articles" style={{ textDecoration: 'none' }}>Articles</Link></li>
                     <li className="nav-list-item"><Link to="/topics" style={{ textDecoration: 'none' }}>Topics</Link></li>
-                    {/* <li className="nav-list-item"><Link to="/users" style={{ textDecoration: 'none' }}>Users</Link></li> */}
                 <li className="navbar-avatar"><Link to="/users"><img src={avatar_url}></img></Link>Logged in as <br></br>{name}</li>
                 </ul>
             </nav>
