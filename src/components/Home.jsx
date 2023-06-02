7a3b63aa302df616afa9039b7790d9b8fab0ae7b
gsaltuk <127306125+gsaltuk@users.noreply.github.com>
2023-06-02 14:58:53 +0100

    move images to public folder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ function Home () {
         <>
         <main>
         <h1 className="home-title">WELCOME TO</h1>
-        <img className="home-logo" src="src/assets/chit-chat-logo-black.svg"/>
+        <img className="home-logo" src="/chit-chat-logo-black.svg"/>
         </main>
         </>
     )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ function Nav({username, name, avatar_url}) {
         <>
             <nav>
                 <ul className="nav-list">
-                    <li className="nav-list-logo"><Link to="" ><img src='./assets/chit-chat-logo-white.svg'></img></Link></li>
+                    <li className="nav-list-logo"><Link to="" ><img src='/chit-chat-logo-white.svg'></img></Link></li>
                     <li className="nav-list-item"><Link to="/articles" style={{ textDecoration: 'none' }}>Articles</Link></li>
                     <li className="nav-list-item"><Link to="/topics" style={{ textDecoration: 'none' }}>Topics</Link></li>
                     {/* <li className="nav-list-item"><Link to="/users" style={{ textDecoration: 'none' }}>Users</Link></li> */}
