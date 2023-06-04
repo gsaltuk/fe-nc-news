6a31776a2cfc68a5d15854692fd5b2b7084e65e3
gsaltuk <127306125+gsaltuk@users.noreply.github.com>
2023-06-04 20:03:09 +0100

    add styling to footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ function Home () {
         <main>
         <h1 className="home-title">WELCOME TO</h1>
         <img className="home-logo" src="/chit-chat-logo-black.svg"/>
+  
         </main>
         </>
     )
