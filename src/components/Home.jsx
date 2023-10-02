108efe233dc05c9cc03264099c4a2a94f026d0cd
gsaltuk <127306125+gsaltuk@users.noreply.github.com>
2023-10-02 08:54:26 +0100

    adjust styling of ol

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ function Home () {
         <main>
         <h1 className="home-title">WELCOME TO</h1>
         <img className="home-logo" src="/chit-chat-logo-black.svg"/>
+        <p className="paragraph">The number one site for hot gossip in the UK!</p>
   
         </main>
         </>
